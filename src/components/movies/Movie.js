3715b421b89f412ab59f5f1ea67fdbb120b9fe42
Danilo Fontes <slipdan@hotmail.com>
2021-08-11 23:27:50 -0300

    Create positioning for the movies and create overview

--- src/components/movies/Movie.js
+++ src/components/movies/Movie.js
@@ -9,8 +9,12 @@ const Movie = ({ title, poster_path, overview, vote_average }) => (
         
         <div className="movie-info">
             <h3>{title}</h3>
-            <span>{vote_average}</span>
-            
+            <span>{vote_average}</span>            
+        </div>
+
+        <div className="movie-over">
+            <h2>Overview:</h2>
+            <p>{overview}</p>
         </div>
     </div>
     );
