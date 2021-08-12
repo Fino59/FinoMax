2693f5b1aac507f0c94f9430ad1585b78b9471cd
Danilo Fontes <slipdan@hotmail.com>
2021-08-12 19:46:31 -0300

    Update the page with CSS and add logo to the header

--- src/App.js
+++ src/App.js
@@ -7,24 +7,61 @@ const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a
 
 function App() {
   const [ movies, setMovies ] = useState([ ]);
+  const [ searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
-    fetch(URL_API)
+    getMovies(URL_API);
+  }, []);
+
+  const getMovies = (API) => {
+    fetch(API)
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
         setMovies(data.results);
       });
-  }, []);
+  }
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    if(searchTerm) {
+      getMovies(SEARCH_API + searchTerm);
+            
+      setSearchTerm('');
+    }
+  };
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value); 
+  };
   
-  return (
+  return (<>
+    <header>
+      
+      <img src='https://i.imgur.com/s1rXDTH.png' className="finomax-logo" alt="FinoMax Logo" />
+
+      <form onSubmit={handleOnSubmit}>
+        <input 
+          className="search" 
+          type="text" 
+          placeholder="Search..."
+          value={searchTerm}
+          onChange={handleOnChange}
+        />
+      </form>
+
+    </header>
+
     <div className="movie-container">
-          {movies.length > 0 && 
-            movies.map((movie) => 
-            <Movie key={movie.id} 
-            {...movie}
-            />)}  
+      {movies.length > 0 && 
+        movies.map((movie) => 
+        <Movie key={movie.id} 
+        {...movie}
+        />)
+      }  
     </div>    
+    </> 
   );
 }
 
