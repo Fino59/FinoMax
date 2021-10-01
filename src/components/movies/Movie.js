4a78cc96aaa091e9e1e47dd5bc4e127a62cccce5
Danilo Fontes <slipdan@hotmail.com>
2021-09-30 21:44:30 -0300

    Update movie component

--- src/components/movies/Movie.js
+++ src/components/movies/Movie.js
@@ -14,17 +14,22 @@ const setVoteClass = (vote) => {
     }
 }
 
-const Movie = ({ title, poster_path, overview, vote_average }) => (
+const Movie = ({ title, release_date, poster_path, overview, vote_average }) => (
     <div className="movie">
         <img src={poster_path ? (IMG_API + poster_path) : 'https://images.unsplash.com/photo-1489599849927-2ee91cede3ba?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80' } alt={title} />
         
         <div className="movie-info">
-            <h3>{title}</h3>
-            <span className={
-                `tag ${setVoteClass(vote_average)}`
-                }>
-                {vote_average}
-            </span>            
+            <div>
+                <h3>{title}</h3>
+                <small>({release_date})</small>
+            </div>
+            <div>
+                <span className={
+                    `tag ${setVoteClass(vote_average)}`
+                    }>
+                    {vote_average}
+                </span>            
+            </div>
         </div>
 
         <div className="movie-over">
